ad15e9f232652a323765295586282bbbdbebc942
Vladislav <qedirast@gmail.com>
2021-03-02 12:28:52 +0700

    Added linter and autoload

---
+++ src/Games/Cli.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Brain\Games\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+class Cli
+{
+    public function start(): void
+    {
+        line('Welcome to the Brain Game!');
+        $name = prompt('May I have your name?');
+        line("Hello, %s!", $name);
+    }
+}
