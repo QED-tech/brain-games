0d96331458494d12474ab18b7b396e8e9eb2be5e
Vladislav <qedirast@gmail.com>
2021-03-07 19:51:04 +0700

    refactor

---
+++ src/Engine.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Brain;
+
+use Brain\Games\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+abstract class Engine
+{
+    protected int $correctCount;
+    protected Cli $cli;
+    protected const SLEEP_TIME = 1;
+    protected const MAX_RETRY = 3;
+    protected string $answer;
+
+    protected function __construct()
+    {
+        $this->correctCount = 0;
+        $this->cli = new Cli();
+    }
+
+    protected function sleep()
+    {
+        sleep(self::SLEEP_TIME);
+    }
+
+    protected function congratulateUser($userName): void
+    {
+        line("Congratulations, %s!", $userName);
+    }
+
+    protected function getAnswer(): void
+    {
+        $this->answer = prompt('Your answer');
+    }
+
+    protected function answerToBoolean(): bool
+    {
+        switch (mb_strtolower($this->answer)) {
+            case 'yes':
+                return true;
+            case 'no':
+                return false;
+            default:
+                return false;
+        }
+    }
+
+    protected function writeRules(string $rules): void
+    {
+        line($rules);
+    }
+
+    protected function checkWin(): void
+    {
+        if ($this->correctCount === self::MAX_RETRY) {
+            $this->congratulateUser($this->cli->getName());
+        }
+    }
+
+    protected function getQuestion($expression): void
+    {
+        $question = "Question: " . $expression;
+        line($question);
+    }
+
+    protected function retryGame(): void
+    {
+        for ($i = 0; $i < self::MAX_RETRY; $i++) {
+            $this->sleep();
+            $this->setExpression();
+            $this->getQuestion($this->getExpression());
+            $this->getAnswer();
+            if (!$this->validateAnswer()) {
+                line("'{$this->answer}' is wrong answer ;(. Correct answer was '{$this->getCorrectAnswer()}'.");
+                line("Let's try again, %s!", $this->cli->getName());
+                break;
+            }
+
+            if ($this->validateAnswer()) {
+                $this->correctCount++;
+                line('Correct!');
+            }
+        }
+    }
+
+    abstract public function process(): void;
+    abstract protected function setExpression(): void;
+    abstract protected function getExpression();
+    abstract protected function validateAnswer(): bool;
+    abstract protected function getCorrectAnswer();
+}
--- src/Games/Even.php
+++ src/Games/Even.php
@@ -2,111 +2,48 @@
 
 namespace Brain\Games;
 
-use function cli\line;
-use function cli\prompt;
+use Brain\Engine;
 
-class Even extends AbstractGame
+class Even extends Engine
 {
-    private Cli $cli;
-    private int $number;
-    private string $answer;
-    private int $correctCount;
-
-    public const MAX_RETRY = 3;
+    private int $expression;
 
     public function __construct()
     {
-        $this->correctCount = 0;
-        $this->cli = new Cli();
+        parent::__construct();
     }
 
-    public function process()
+    public function process(): void
     {
         $this->cli->greetingUser();
         $this->sleep();
-        $this->writeRules();
+        $this->writeRules('Answer "yes" if the number is even, otherwise answer "no".');
         $this->retryGame();
         $this->checkWin();
     }
 
-    private function retryGame(): void
-    {
-        for ($i = 0; $i < self::MAX_RETRY; $i++) {
-            $this->sleep();
-            $this->getQuestion();
-            $this->getAnswer();
-            if (!$this->validateAnswer()) {
-                line("'{$this->answer}' is wrong answer ;(. Correct answer was '{$this->getCorrectAnswer()}'.");
-                line("Let's try again, %s!", $this->cli->getName());
-                break;
-            }
-
-            if ($this->validateAnswer()) {
-                $this->correctCount++;
-                line('Correct!');
-            }
-        }
-    }
-
-    private function checkWin(): void
-    {
-        if ($this->correctCount === self::MAX_RETRY) {
-            $this->congratulateUser($this->cli->getName());
-        }
-    }
-
-    private function getCorrectAnswer()
+    protected function getCorrectAnswer(): string
     {
         return $this->answerToBoolean() === true ? 'no' : 'yes';
     }
 
-
-    private function writeRules(): void
-    {
-        line('Answer "yes" if the number is even, otherwise answer "no".');
-    }
-
-    private function getAnswer(): void
-    {
-        $this->answer = prompt('Your answer');
-    }
-
-    private function validateAnswer(): bool
+    protected function validateAnswer(): bool
     {
         return $this->answerToBoolean() === $this->isEven();
     }
 
-    private function answerToBoolean(): bool
-    {
-        switch (mb_strtolower($this->answer)) {
-            case 'yes':
-                return true;
-            case 'no':
-                return false;
-            default:
-                return false;
-        }
-    }
-
     private function isEven(): bool
     {
-        return $this->number % 2 === 0;
+        return $this->expression % 2 === 0;
     }
 
-    private function getQuestion(): void
+    protected function setExpression(): void
     {
-        $this->setNumber();
-        $question = "Question: " . $this->getNumber();
-        line($question);
+        $this->expression = mt_rand(3, 22);
     }
 
-    private function setNumber(): void
+    protected function getExpression(): int
     {
-        $this->number = mt_rand(3, 22);
-    }
-
-    private function getNumber(): int
-    {
-        return $this->number;
+        return $this->expression;
     }
 }
